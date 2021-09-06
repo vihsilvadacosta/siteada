6501c6a049b3964d00b564deb215890d53f0fcd7
vihsilvadacosta <87202281+vihsilvadacosta@users.noreply.github.com>
2021-09-06 19:42:30 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+  if(document.getElementById("Nome").value != "" && document.getElementById("E-mail").value != ""){
+    alert("Prontinho! Você receberá as novidades por email.")
+  }else{
+    alert("Por favor, preencha os campos nome e email!")
+  }
+
